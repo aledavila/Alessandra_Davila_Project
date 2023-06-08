b88046f0ed19543beb3ea6d5bd8f423121abc3a6
Alessandra Davila <adavila91@gmail.com>
2023-06-08 01:13:24 -0500

    feat: initialize app and adds jest and axe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+import React from 'react'
+import reportAccessibility from '../utils/reportAccessibility'
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+reportAccessibility(React)
